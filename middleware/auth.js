28184d9f72956954217a56e3cf9786e8afb02a67
lokesh_d~ <dlokesh504@gmail.com>
2024-12-18 21:48:30 +0530

    resole

--- middleware/auth.js
+++ middleware/auth.js
@@ -11,9 +11,7 @@ admin.initializeApp({
     client_id: '109012306460102602556',
     auth_uri: "https://accounts.google.com/o/oauth2/auth",
     token_uri: "https://oauth2.googleapis.com/token",
-    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-    client_x509_cert_url: process.env.GOOGLE_CLIENT_CERT_URL
-  }),
+    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs"  }),
 });
 
 module.exports = async (req, res, next) => {
