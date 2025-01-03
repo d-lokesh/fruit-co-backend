99bb923dc7f3a7bed6bdd06cc8813d5229de29fb
lokesh_d~ <dlokesh504@gmail.com>
2025-01-03 16:54:53 +0530

    add port from system

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ console.error('Error initializing WhatsApp client:', error);
 
 
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
